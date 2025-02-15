cc99d083c5079171aba7a897ab6de6cb3e93dd29
URSUS32 <saul.razo@iteso.mx>
2025-02-15 10:59:20 -0600

    update

--- p1/init.c
+++ p1/init.c
@@ -1,81 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <signal.h>
 
-#define NUM_GETTY 6  // Número de procesos getty a crear
-
-// Array para almacenar los PID de los procesos getty
-pid_t getty_pids[NUM_GETTY];
-
-// Función para manejar la señal de shutdown
-void shutdown_handler(int signum) {
-    if (signum == SIGUSR1) {
-        printf("Init: Received shutdown signal. Terminating all processes...\n");
-        // Terminar todos los procesos getty
-        for (int i = 0; i < NUM_GETTY; i++) {
-            if (getty_pids[i] > 0) {
-                kill(getty_pids[i], SIGTERM);  // Enviar señal de terminación
-            }
-        }
-        exit(0);  // Terminar init
-    }
-}
-
-// Función para crear un proceso getty en una nueva ventana xterm
-void create_getty(int index) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("Fork failed");
-        exit(1);
-    } else if (pid == 0) {
-        // Proceso hijo: ejecutar getty en una nueva ventana xterm
-        char *args[] = {"xterm", "-e", "./getty", NULL};
-        execvp("xterm", args);
-        // Si execvp falla
-        perror("execvp failed");
-        exit(1);
-    } else {
-        // Proceso padre: almacenar el PID del proceso getty
-        getty_pids[index] = pid;
-        printf("Init: Created getty process with PID %d\n", pid);
-    }
-}
-
 int main() {
-    printf("Init: Starting init process (PID: %d)\n", getpid());
+    printf("Hola");
 
-    // Registrar el manejador de la señal de shutdown
-    signal(SIGUSR1, shutdown_handler);
-
-    // Crear los 6 procesos getty
-    for (int i = 0; i < NUM_GETTY; i++) {
-        create_getty(i);
-    }
-
-    // Monitorear los procesos getty
-    while (1) {
-        int status;
-        pid_t terminated_pid = waitpid(-1, &status, WNOHANG);  // No bloquear
-
-        if (terminated_pid > 0) {
-            // Un proceso getty terminó
-            printf("Init: Getty process with PID %d terminated.\n", terminated_pid);
-
-            // Encontrar el índice del proceso terminado
-            for (int i = 0; i < NUM_GETTY; i++) {
-                if (getty_pids[i] == terminated_pid) {
-                    // Crear un nuevo proceso getty en su lugar
-                    create_getty(i);
-                    break;
-                }
-            }
-        }
-
-        sleep(1);  // Esperar 1 segundo antes de verificar nuevamente
-    }
-
+    
     return 0;
 }
--- p1/sh.c
+++ p1/sh.c
@@ -1,56 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <signal.h>
-#include <sys/wait.h>
-
-// Función para ejecutar un comando en primer o segundo plano
-void execute_command(char *command, int background) {
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("Fork failed");
-        return;
-    } else if (pid == 0) {
-        // Proceso hijo: ejecutar el comando
-        char *args[] = {"/bin/sh", "-c", command, NULL};
-        execvp("/bin/sh", args);
-        // Si execvp falla
-        perror("execvp failed");
-        exit(1);
-    } else if (!background) {
-        // Proceso padre: esperar si es en primer plano
-        wait(NULL);
-    }
-}
 
 int main() {
-    char input[256];
+    printf("Hola");
 
-    while (1) {
-        // Mostrar el prompt
-        printf("sh > ");
-        fgets(input, sizeof(input), stdin);
-        input[strcspn(input, "\n")] = 0;  // Eliminar el salto de línea
-
-        // Comandos especiales
-        if (strcmp(input, "exit") == 0) {
-            printf("Exiting shell...\n");
-            break;
-        } else if (strcmp(input, "shutdown") == 0) {
-            printf("Initiating shutdown...\n");
-            kill(getppid(), SIGUSR1);  // Enviar señal de shutdown a init
-            break;
-        }
-
-        // Ejecutar comando en primer o segundo plano
-        int background = (input[strlen(input) - 1] == '&');
-        if (background) {
-            input[strlen(input) - 1] = 0;  // Eliminar el '&'
-        }
-        execute_command(input, background);
-    }
-
+    
     return 0;
 }
